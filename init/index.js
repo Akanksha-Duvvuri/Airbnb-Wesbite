3182a01e138fd182e2de316b6410196594145fa4
Akanksha Duvvuri <akanksha.duvvuri16@gmail.com>
2026-05-02 13:41:46 +0530

    sample data added

---
+++ init/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import data from './data.js'
+import Listing from "../models/listing.js";
+
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main().then(() => {
+    console.log("connected successfully");
+}) .catch((err) => {
+    console.log(err);
+});
+
+async function main(){
+    await mongoose.connect(MONGO_URL);
+};

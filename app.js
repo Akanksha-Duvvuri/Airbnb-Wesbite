702095d6dec900d428cac6af1144864641f2dbdc
Akanksha Duvvuri <akanksha.duvvuri16@gmail.com>
2026-05-02 13:36:04 +0530

    first insertion to db

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import Listing from './models/listing.js'
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -11,15 +12,29 @@ main().then(() => {
 
 async function main(){
     await mongoose.connect(MONGO_URL);
-}
+};
 
 const app = express();
 const port = 8080;
 
 app.get("/", (req, res) => {
     res.send("root dir working");
-})
+});
+
+app.get("/testListing", async (req, res) => {
+    let sampleListing = new Listing({
+        title: "my new villa",
+        description: "by the beach",
+        price: 1200,
+        location: "Goa",
+        country: "india",
+    });
+
+    await sampleListing.save();
+    console.log("sample was saved");
+    res.send("successfully saved");
+});
 
 app.listen(port, "127.0.0.1", () => {
     console.log(`listening on port ${port}`);
-})
+});
